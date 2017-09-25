bd75b6106811d8c94bd492df9ce3b22a118620c2
daipeng <daipeng@doctorwork.com>
2017-09-25 10:40:00 +0800

    监控append型日志输出

--- modules/fs/watchLog-demo.js
+++ modules/fs/watchLog-demo.js
@@ -1,42 +1,45 @@
 /**
- * 监控日志改动
+ * 监控日志改动，因为日志都是append型的文件修改方式，所以只需要传控最后一排的变化就行
  */ 
 const fs = require( 'fs' );
 const readline = require( 'readline' );
 let logPath = './log.js';
 let getLog = './getlog.js';
-let init = ()=>{
 
-}
-
-let conoleLog = ()=>{
-
-}
 fs.open( logPath, 'a+', (err, fd)=>{
-    watchChage( logPath );
+    watchChage( logPath, fd );
 } );
 
 // 监控文件变化
-let watchChage = ( filename )=>{
+let watchChage = ( filename, fd )=>{
     return fs.watchFile( filename, {
         persistent : true,
         interval : 1000 
     }, (curr, prev)=>{
+        let _len = curr.size - prev.size;
+        let buffer = Buffer.alloc( _len );
+        if( curr.mtime > prev.mtime ){
+            fs.read( fd, buffer, 0, _len,prev.size, (err, bytesRead, buffer)=>{
+                console.log( buffer.toString() );
+            } );
+        }else{
+            console.log( '读取文件错误' );
+        }
         
     } );
 }
 
-// 逐行读取
-readline.createInterface( {
-    input : fs.createReadStream(logPath, {
-        flags : 'r',
-        autoClose : true,
-        fd : null
-    }),
-    output : fs.createWriteStream(getLog, {
+// 逐行读取，转移到指定文件
+// readline.createInterface( {
+//     input : fs.createReadStream(logPath, {
+//         flags : 'r',
+//         autoClose : true,
+//         fd : null
+//     }),
+//     output : fs.createWriteStream(getLog, {
 
-    })
-} );
+//     })
+// } );
 
 
 
